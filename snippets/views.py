2eac1b773a48e179825a80ecbb549d41e3c66529
aniket21mathur <amathur@ec.iitr.ac.in>
2018-10-17 07:01:08 +0530

    tutorial4

--- snippets/views.py
+++ snippets/views.py
@@ -2,10 +2,13 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer,UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 #Use to bypass django csrf token for verification.
 '''	
 class SnippetList(APIView):
@@ -51,12 +54,30 @@ class SnippetDetail(APIView):
 '''		
 
 class SnippetList(generics.ListCreateAPIView):
+
 	queryset=Snippet.objects.all()
 	serializer_class=SnippetSerializer
+	#That comma is neccesary.
+	permission_classes= (permissions.IsAuthenticatedOrReadOnly,)
+	def perform_create(self, serializer):
+		serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset=Snippet.objects.all()
 	serializer_class=SnippetSerializer
+	permission_classes= (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+	queryset=User.objects.all()
+	serializer_class=UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+	queryset=User.objects.all()
+	serializer_class=UserSerializer
+
+
 
 		
 		
